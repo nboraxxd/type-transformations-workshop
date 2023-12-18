9b7767ba7575cf567ce7283a93a5dd49c87effa9
bora <nhatbaoxxd@gmail.com>
2023-12-18 19:50:43 +0700

    Ex 06. Extracting members of a Union

--- src/02-unions-and-indexing/06-extract-from-discriminated-union.problem.ts
+++ src/02-unions-and-indexing/06-extract-from-discriminated-union.problem.ts
@@ -1,19 +1,32 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils'
 
 export type Event =
   | {
       type: "click";
       event: MouseEvent;
+      what: "Table"
     }
   | {
       type: "focus";
       event: FocusEvent;
+      what: "Table"
     }
   | {
       type: "keydown";
       event: KeyboardEvent;
-    };
+      how: "Fast"
+    }
 
-type ClickEvent = unknown;
+type ClickEvent = Extract<Event, { what: 'Table' }>
 
-type tests = [Expect<Equal<ClickEvent, { type: "click"; event: MouseEvent }>>];
+type Fruit = 'Apple' | 'Orange' | 'Banana'
+type BananaAndOrange = Extract<Fruit, 'Banana' | 'Orange'>
+
+type tests = [
+  Expect<
+    Equal<
+      ClickEvent,
+      { type: 'click'; event: MouseEvent; what: 'Table' } | { type: 'focus'; event: FocusEvent; what: 'Table' }
+    >
+  >
+]
