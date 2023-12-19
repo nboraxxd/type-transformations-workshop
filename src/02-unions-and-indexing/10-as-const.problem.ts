6a332c7751627cc9acd34d3c00fb95c55bb52815
bora <nhatbaoxxd@gmail.com>
2023-12-20 00:36:58 +0700

    Ex10. Resolve an object's values as Literal types

--- src/02-unions-and-indexing/10-as-const.problem.ts
+++ src/02-unions-and-indexing/10-as-const.problem.ts
@@ -1,17 +1,44 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils'
 
 /**
  * Some docs that might help!
  * https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html#const-assertions
  */
+
+/**
+ * Sự khác nhau giữa `as const` và `Object.freeze` trong Typescript là:
+ * `as const` read-only luôn cả deeper properties
+ * `Object.freeze` chỉ làm read-only shallow properties
+ */
+
+const something = {
+  foo: 'foo',
+  bar: 'bar',
+  baz: 'baz',
+  log: {
+    info: 'hello',
+    error: 'hi',
+  },
+} as const
+
+const somethingElse = Object.freeze({
+  foo: 'foo',
+  bar: 'bar',
+  baz: 'baz',
+  log: {
+    info: 'hello',
+    error: 'hi',
+  },
+})
+
 export const programModeEnumMap = {
-  GROUP: "group",
-  ANNOUNCEMENT: "announcement",
-  ONE_ON_ONE: "1on1",
-  SELF_DIRECTED: "selfDirected",
-  PLANNED_ONE_ON_ONE: "planned1on1",
-  PLANNED_SELF_DIRECTED: "plannedSelfDirected",
-};
+  GROUP: 'group',
+  ANNOUNCEMENT: 'announcement',
+  ONE_ON_ONE: '1on1',
+  SELF_DIRECTED: 'selfDirected',
+  PLANNED_ONE_ON_ONE: 'planned1on1',
+  PLANNED_SELF_DIRECTED: 'plannedSelfDirected',
+} as const
 
 export type GroupProgram = typeof programModeEnumMap["GROUP"];
 export type AnnouncementProgram = typeof programModeEnumMap["ANNOUNCEMENT"];
